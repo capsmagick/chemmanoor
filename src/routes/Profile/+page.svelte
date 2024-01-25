15e713311426470d8db735b4b0b90bd94b904734
royid_poulose <royidcp96@gmail.com>
2024-01-25 15:42:08 +0530

    profile updates. new routes created and form data changes

--- src/routes/Profile/+page.svelte
+++ src/routes/Profile/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
     import type { PageData } from './$types';
+    import UserDetails from './userDetails.svelte'
     
     export let data: PageData;
 </script>
+<div>
+    <UserDetails { data }/>
+</div>
---
+++ src/routes/Profile/ChildrensDetails/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    import ChildrenDetailsForm from './childrenDetailsForm.svelte';
+    
+    export let data: PageData;
+</script>
+
+<div class="mt-30">
+    <ChildrenDetailsForm { data }/> 
+</div>
